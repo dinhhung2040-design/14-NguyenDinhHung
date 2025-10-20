2f22cdfacfff606fd1753f4a22bd2affb24c3ff7
Nguyen Dinh Hung <dinhhung2040@gmail.com>
2025-10-20 22:13:23 +0700

    feat: Implement add student function

---
+++ 14-NguyenDinhHung.py
@@ -0,0 +1,24 @@
+student_list = []
+
+def add_student(name, year_of_birth, address):
+    """
+    YÊU CẦU 1: Hoàn thiện hàm này.
+    - Tạo một dictionary để lưu thông tin sinh viên.
+    - Thêm dictionary đó vào danh sách `student_list`.
+    - In ra thông báo "Da them sinh  vien <ten> thanh cong."
+    """
+    ### VIẾT CODE CỦA BẠN VÀO ĐÂY ###
+    student = {
+        "name": name,
+        "year_of_birth": year_of_birth,
+        "address": address
+    }
+    student_list.append(student)
+    print(f"Da them sinh  vien {name} thanh cong.")
+
+if __name__ == "__main__":
+    print("--- CHUONG TRINH QUAN LY SINH VIEN ---")
+    print("\n1. Them sinh vien:")
+    add_student("Nguyen Van An", 2003, "Da Nang")
+    add_student("Tran Thi Binh", 2002, "Quang Nam")
+    add_student("Le Van Hung", 2003, "Hue")
